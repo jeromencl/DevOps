6ec2f8c469603e51c851d465ccf19a3d368dfe56
Jerome Ng <jerome_ncl@hotmail.com>
2018-12-19 07:30:45 +0000

    server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var app = express();
 
 // Routes
 app.get('/', function (req, res) {
-  res.send('Hello World! [port' + this.address().port + ']');
+  res.send('Hello World! [Cloud and Ground]');
 });
 
 // Create the server
